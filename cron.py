0971cedb4f24a84c53eb00e7f90402de5afb26b8
Vaibhav Verma <vaibhav2614@gmail.com>
2012-08-09 15:19:15 -0400

    Adding an email address and a notification time
    Fixing up the UI
    
    Adding stuff for migration

--- cron.py
+++ cron.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python
 
 import requests
-from models import db, Snipe, Course, Section
+from models import db, Snipe
+from soc import Soc
 
-def poll(department):
+soc = Soc()
 
+def poll(subject):
+    courses = soc.get_courses(subject)
 
+    print "Subject: ", subject
+    open_data = {}
+    for course in courses:
+        course_number = course['courseNumber']
+        open_data[course_number] = []
+        for section in course['sections']:
+            # section is open
+            if section['openStatus']:
+                if course_number in open_data:
+                    open_data[course_number].append(section['number'])
+
+        print 'Course: %s Data: %s' %(course_number, open_data[course_number])
+
+    # all of these courses are open
+    open_courses = [course for course, open_sections in open_data.iteritems() if open_sections]
+
+    snipes = Snipe.query.filter(Snipe.course_number.in_(open_courses))
+    for snipe in snipes:
+        if snipe.section in open_data[snipe.course_number]:
+            notify(snipe)
+
+def notify(snipe):
+    print snipe
 
 if __name__ == '__main__':
     # get all the courses that should be queried.
-    courses = Course.query.all()
-    for course in courses:
-        poll(course)
+    subjects = db.session.query(Snipe.subject).distinct().all()
+    for subject in subjects:
+        poll(subject)
