504427c5f9a6706f0f8a5a87c0a33ef842b3b345
Vaibhav Verma <vaibhav2614@gmail.com>
2012-08-09 17:59:14 -0400

    Adding text messaging and email notification functionality
    Finishing up cronjob

--- cron.py
+++ cron.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
+from flaskext.mail import Message, Mail
+from twilio.rest import TwilioRestClient
 
 import requests
 from models import db, Snipe
 from soc import Soc
+from app import mail
 
 soc = Soc()
+
+account = "ACd35259f50af04485caaa4c69b779418b"
+token = "a918ed53d5c28a74c0abeb911c7f63af"
+
+client = TwilioRestClient(account=account, token=token)
+
+EMAIL_SENDER = "sniper@vverma.net"
 
 def poll(subject):
     courses = soc.get_courses(subject)
 
-    print "Subject: ", subject
     open_data = {}
     for course in courses:
         course_number = course['courseNumber']
@@ -20,8 +29,6 @@ def poll(subject):
                 if course_number in open_data:
                     open_data[course_number].append(section['number'])
 
-        print 'Course: %s Data: %s' %(course_number, open_data[course_number])
-
     # all of these courses are open
     open_courses = [course for course, open_sections in open_data.iteritems() if open_sections]
 
@@ -31,7 +38,20 @@ def poll(subject):
             notify(snipe)
 
 def notify(snipe):
-    print snipe
+    course = '%s:%s-%s' % (snipe.subject, snipe.course_number, snipe.section)
+    text = 'A course (%s) that you were watching looks open. Go register for it! If you don\'t get in, reply back with "%s" and I\'ll continue watching it' % (course, course)
+
+    if snipe.email:
+
+        message = Message('[Course Sniper](%s) is open' %(course), sender=EMAIL_SENDER)
+        message.body = text
+        message.add_recipient(snipe.email)
+
+        mail.send(message)
+
+    if snipe.phone_number:
+        message = client.sms.messages.create(to=snipe.phone_number, from_="+17328527245", body=text)
+        
 
 if __name__ == '__main__':
     # get all the courses that should be queried.
