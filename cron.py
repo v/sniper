4ce4e2bdc038d5a260b05c6d16223fa55c522802
Vaibhav Verma <vaibhav2614@gmail.com>
2012-11-12 11:47:34 -0800

    Found the bug.
    Wasn't checking subject when polling

--- cron.py
+++ cron.py
@@ -46,7 +46,7 @@ def poll(subject, result=False):
 
     if open_courses:
         # Notify people that were looking for these courses
-        snipes = Snipe.query.filter(Snipe.course_number.in_(open_courses))
+        snipes = Snipe.query.filter(Snipe.course_number.in_(open_courses), Snipe.subject==subject)
         for snipe in snipes:
             if snipe.section in open_data[snipe.course_number]:
                 print "FUCK LIFE\n\n"
